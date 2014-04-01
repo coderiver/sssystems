141281abc4208ddf2b8bf6274f707c36059c27b3
Valera19900 <kaktuz34@gmail.com>
2014-04-01 18:34:27 +0300

    fixed_50%

--- js/common.js
+++ js/common.js
@@ -1,9 +1,49 @@
 $(document).ready(function() {
 
-	$('.js-shop').click(function() {
-        $(".js-services").slideToggle();
+	$(document).click(function() {
+		$(".js-popup-field").hide();
+		$(".js-btn-sbm").parent().addClass("is-active");
+	});
+		$(".js-popup").on("click", function(event){
+			event.stopPropagation();
+		});
+		$(".js-popup-close").on("click", function(event){
+				$(".js-popup-field").hide();
+				event.stopPropagation();
+		});
+		$('.js-btn').click(function() {
+				$(".js-popup-field").show();
+				return false;
+		});
+
+	// $('.js-shop').click(function() {
+ //        $(".js-services").slideToggle();
+ //        return false;
+ //    });
+
+	$('.js-services__oll').click(function() {
+        $(".js-attendance").slideToggle();
         return false;
     });
+	
+	$(".fancybox-effects").fancybox({
+			wrapCSS    : 'fancybox-custom',
+			closeClick : true,
+
+			openEffect : 'none',
+
+			helpers : {
+				title : {
+					type : 'inside'
+				},
+				overlay : {
+					css : {
+						'background' : 'rgba(238,238,238,0.85)'
+					}
+				}
+			}
+		});
 
 });
 
+$('.fancybox').fancybox();
