d53f70cbceed26696a4a5d4858d918914a41d13c
Valera19900 <kaktuz34@gmail.com>
2014-04-02 13:54:07 +0300

    fixed

--- js/common.js
+++ js/common.js
@@ -44,6 +44,50 @@ $(document).ready(function() {
 			}
 		});
 
+	$('.js_loginform').each(function() {
+
+    $(this).validate({
+	 
+			rules:{
+
+				name:{
+						required: true,
+				},
+				
+			  phone:{
+			      required: true,
+			  },
+
+			  mail:{
+			      required: true,
+			  },
+
+			},
+
+    });
+
+	});
+
 });
 
+
+
 $('.fancybox').fancybox();
+
+function js_numbers(input) { 
+  var value = input.value; 
+  var rep = /[-\.;":'a-zA-Zа-яА-Я]/; 
+  if (rep.test(value)) { 
+      value = value.replace(rep, ''); 
+      input.value = value; 
+  } 
+}
+
+function js_mail(input) { 
+    var value = input.value; 
+    var rep = /[а-яА-Я]/; 
+    if (rep.test(value)) { 
+        value = value.replace(rep, ''); 
+        input.value = value; 
+    } 
+	}
